fff0ae03dbbab3714b44687ebab4ba856fa4f1d4
Gaetan Godin <gaetgodi@gmail.com>
2026-07-22 16:54:57 -0400

    Media Curator: remove autocomplete=off so datalist popup shows

--- inc/media-curator.php
+++ inc/media-curator.php
@@ -143,7 +143,7 @@ function fbl_media_curator_render_page() {
 
             <label>Copy selected to:
             <input type="text" id="fbl-curator-target" name="fbl-curator-target" value="WEB_" size="40"
-                       list="fbl-curator-targets" autocomplete="off" />
+                       list="fbl-curator-targets" />
                        <datalist id="fbl-curator-targets">
                     <?php foreach ($folders as $f): ?>
                         <option value="<?php echo esc_attr($f->name); ?>"><?php echo esc_html($f->name); ?></option>
