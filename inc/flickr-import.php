bf5f0a3551121a7d0739c37d0c4979067733a42b
Gaetan Godin <gaetgodi@gmail.com>
2026-07-23 20:58:50 -0400

    Create FileBird folders unowned (created_by=0) so they are visible to all users

--- inc/flickr-import.php
+++ inc/flickr-import.php
@@ -63,7 +63,7 @@ function fbl_fi_create_folder($name) {
     global $wpdb;
     $wpdb->insert(
         "{$wpdb->prefix}fbv",
-        array('name' => $name, 'parent' => 0, 'type' => 0, 'ord' => 0, 'created_by' => get_current_user_id()),
+        array('name' => $name, 'parent' => 0, 'type' => 0, 'ord' => 0, 'created_by' => 0),
         array('%s', '%d', '%d', '%d', '%d')
     );
     return (int) $wpdb->insert_id;
